78768c7bcab34bdb41f905fedd98d7010b7378f7
enalmarzuki <enalmarzuki07@gmail.com>
2025-04-08 00:35:35 +0800

    feat: update lint staged

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 /* eslint-env node */
 const path = require('path');
-const formatCommand = 'prettier . --check';
+const formatCommand = ['prettier --write .', 'prettier . --check'];
 
 module.exports = {
   '*': formatCommand,
